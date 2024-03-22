4b67a69f621ce7611ca4e44dabd0023c05fc7bca
MarcoGirelaVida <marcogirelavida@gmail.com>
2024-03-22 03:46:57 +0100

    Tutorial done

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -2,10 +2,120 @@
 #include <iostream>
 using namespace std;
 
+#include <cstdlib> // Add missing include directive for the 'cstdlib' library
+void PonerTerrenoEnMatriz(const vector<unsigned char> terreno, const state current_state, vector< vector< unsigned char> > &mapaResultado)
+{
+	const vector<pair<short, short>> pos_norte = { {0, 0}, {-1, -1}, {-1, 0}, {-1, 1}, {-2, -2}, {-2, -1}, {-2, 0}, {-2, 1}, {-2, 2}, {-3, -3}, {-3, -2}, {-3, -1}, {-3, 0}, {-3, 1}, {-3, 2}, {-3, 3} };
+	const vector<pair<short, short>> pos_noreste = { {0, 0}, {-1, 0}, {-1, 1}, {0, 1}, {-2, 0}, {-2, 1}, {-2, 2}, {-1, 2}, {0, 2}, {-3, 0}, {-3, 1}, {-3, 2}, {-3, 3}, {-2, 3}, {-1, 3}, {0, 3} };
+	vector<pair<short, short>> vector_pos;
+	short posfil, poscol;
+
+	switch (current_state.brujula)
+	{
+	case norte: 	posfil = 1; 	poscol = 1;		vector_pos = pos_norte; 	break;
+	case noreste: 	posfil = 1; 	poscol = 1; 	vector_pos = pos_noreste; 	break;
+	case este: 		posfil = -1; 	poscol = 1;		vector_pos = pos_norte;		break;
+	case sureste: 	posfil = -1; 	poscol = 1; 	vector_pos = pos_noreste;	break;
+	case sur: 		posfil = -1; 	poscol = -1; 	vector_pos = pos_norte;		break;
+	case suroeste: 	posfil = -1; 	poscol = -1; 	vector_pos = pos_noreste;	break;
+	case oeste: 	posfil = 1; 	poscol = -1; 	vector_pos = pos_norte;		break;
+	case noroeste: 	posfil = 1; 	poscol = -1; 	vector_pos = pos_noreste; 	break;
+	}
+
+	for (size_t i = 0; i < 16; i++)
+	 {
+		unsigned fil = current_state.fil + vector_pos[i].first*posfil;
+		unsigned col = current_state.col + vector_pos[i].second*poscol;
+		if (terreno[i] == 'G' || terreno[i] == 'B' || terreno[i] == 'R' || terreno[i] == 'Z')
+			casillas_objetivo[{fil, col}] = terreno[i];
+		else
+			casillas_objetivo.erase({fil, col});
+
+		mapaResultado[fil][col] = terreno[i];
+	 }
+}
+
+void GenerarMapaPotencial (const unordered_map<pair<unsigned, unsigned>, unsigned char> casillas_objetivo, vector<vector<unsigned char>> & mapaResultado)
+{
+	queue<pair<unsigned, unsigned>> restantes;
+}
 Action ComportamientoJugador::think(Sensores sensores)
 {
+	Action accion = actIDLE;	
 
-	Action accion = actIDLE;
+	// Actualización según última acción
+	switch (last_action)
+	{
+	case actWALK:
+		switch (current_state.brujula)
+		{
+			case norte: current_state.fil -= walk_stepsize; break;
+			case noreste: current_state.fil -= walk_stepsize; current_state.col += walk_stepsize; break;
+			case este: current_state.col += walk_stepsize; break;
+			case sureste: current_state.fil += walk_stepsize; current_state.col += walk_stepsize; break;
+			case sur: current_state.fil += walk_stepsize; break;
+			case suroeste: current_state.fil += walk_stepsize; current_state.col -= walk_stepsize; break;
+			case oeste: current_state.col -= walk_stepsize; break;
+			case noroeste: current_state.fil -= walk_stepsize; current_state.col -= walk_stepsize; break;
+		}
+		break;
+
+	case actRUN:
+		switch (current_state.brujula)
+		{
+			case norte: current_state.fil -= run_stepsize; break;
+			case noreste: current_state.fil -= run_stepsize; current_state.col += run_stepsize; break;
+			case este: current_state.col += run_stepsize; break;
+			case sureste: current_state.fil += run_stepsize; current_state.col += run_stepsize; break;
+			case sur: current_state.fil += run_stepsize; break;
+			case suroeste: current_state.fil += run_stepsize; current_state.col -= run_stepsize; break;
+			case oeste: current_state.col -= run_stepsize; break;
+			case noroeste: current_state.fil -= run_stepsize; current_state.col -= run_stepsize; break;
+		}
+		break;
+
+	case actTURN_SR:
+		// Girar 45º derecha
+		current_state.brujula = static_cast<Orientacion> ((current_state.brujula+1)%8);
+		break;
+	
+	case actTURN_L:
+		// Girar 90º izquierda
+		current_state.brujula = static_cast<Orientacion> ((current_state.brujula+6)%8);
+		break;
+	}
+
+	if (sensores.terreno[0] == 'G' and !bien_situado)
+	{
+		current_state.fil = sensores.posF;
+		current_state.col = sensores.posC;
+		current_state.brujula = sensores.sentido;
+		bien_situado = true;
+	}
+
+	if (bien_situado)
+	{
+		PonerTerrenoEnMatriz(sensores.terreno, current_state, mapaResultado);
+	}
+	
+	// Decidir nueva acción
+	if ((sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S' or
+		sensores.terreno[2] == 'G') and sensores.agentes[2] == '_')
+	{
+		accion = actWALK;
+	}
+	else if (!girar_derecha)
+	{
+		accion = actTURN_L;
+		girar_derecha = (rand()%2 == 0);
+	}
+	else
+	{
+		accion = actTURN_SR;
+		girar_derecha = (rand()%2 == 0);
+	}
+	
+	last_action = accion;
 
 	// Mostrar el valor de los sensores
 	cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC;
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -2,14 +2,34 @@
 #define COMPORTAMIENTOJUGADOR_H
 
 #include "comportamientos/comportamiento.hpp"
+#include <unordered_map>
+#include <queue>
 using namespace std;
 
+struct state{
+  int fil;
+  int col;
+  Orientacion brujula;
+};
+
+const unsigned short walk_stepsize = 1, run_stepsize = 2;
 class ComportamientoJugador : public Comportamiento{
 
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
       // Dar el valor inicial a las variables de estado
+      current_state.fil = 99;
+      current_state.col = 99;
+      current_state.brujula = norte;
+
+      girar_derecha = false;
+      bien_situado = false;
+
+      for (size_t i = 0; i < mapaResultado.size(); i++)
+        for (size_t j = 0; j < mapaResultado.size(); j++)
+          if (mapaResultado[i][j] == '?')
+            casillas_objetivo.insert({{i, j}, '?'});
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -20,5 +40,9 @@ class ComportamientoJugador : public Comportamiento{
 
   private:
   // Declarar aquí las variables de estado
+  state current_state; Orientacion brujula;
+  Action last_action;
+  bool girar_derecha, bien_situado;
+  std::unordered_map<pair<unsigned, unsigned>, unsigned char> casillas_objetivo;
 };
 #endif
