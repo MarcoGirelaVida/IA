24993bc9acdc368d367efed09457c64522743dde
MarcoGirelaVida <marcogirelavida@gmail.com>
2024-04-05 01:44:59 +0200

    great advances

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -1,71 +1,142 @@
 #include "../Comportamientos_Jugador/jugador.hpp"
-#include <iostream>
+#include <iostream>interesantes
 using namespace std;
 
-#include <cstdlib> // Add missing include directive for the 'cstdlib' library
+#include <cstdlib>
 
-bool ComportamientoJugador::es_casilla_interesante(const casilla c)
+bool ComportamientoJugador::escanear_casilla(const casilla c)
 {
 	switch (mapaResultado[c.first][c.second])
 	{
+	// Bosque
+	case 'B': (*mapa_potencial_actual)[c.first][c.second] = zapatillas ? -5 : -25; return false;
+	// Agua
+	case 'A': (*mapa_potencial_actual)[c.first][c.second] = bikini ? -5 : -50; return false;
+	// Precipicio
+	case 'P': (*mapa_potencial_actual)[c.first][c.second] = 0; return false;
+	// Piedra
+	case 'S': (*mapa_potencial_actual)[c.first][c.second] = -1; return false;
+	// Arena
+	case 'T': (*mapa_potencial_actual)[c.first][c.second] = -2; return false;
+	// Muro
+	case 'M': (*mapa_potencial_actual)[c.first][c.second] = 0; return false;
+	
 	// Casilla de ubicación
-	case '':
-		casillas_interesantes.emplace(c, 3);
-		break;
-
+	case 'G': (*mapa_potencial_actual)[c.first][c.second] = ubicado ? -1 : 5000; if (!ubicado) casillas_interesantes.push(c); return true;
 	// Bikini
-	case '':
-		casillas_interesantes.emplace(c, 2);
-		break;
-
+	case 'K': (*mapa_potencial_actual)[c.first][c.second] = bikini ? -1 : 3000; if(!bikini) casillas_interesantes.push(c); return true;
 	// Zapatillas
-	case '':
-		casillas_interesantes.emplace(c, 1);
-		break;
-
-	default:
-		return false;
+	case 'D': (*mapa_potencial_actual)[c.first][c.second] = zapatillas ? -1 : 3000; if(!zapatillas) casillas_interesantes.push(c); return true;
+	// Recarga
+	case 'X': (*mapa_potencial_actual)[c.first][c.second] = prioridad_recarga; casillas_interesantes.push(c); return true;
+	// Desconocida
+ 	case '?': (*mapa_potencial_actual)[c.first][c.second] = 1; casillas_desconocidas.push(c); return true;
 	}
-
-	return true; 
+	
 }
 
-bool ComportamientoJugador::es_casilla_desconocida(const casilla c)
-{
-	if (mapaResultado[c.first][c.second] == '?')
-	{
-		casillas_desconocidas.push(c);
-		return true;
-	}
-
-	return false;
-}
-
-bool ComportamientoJugador::buscar_casillas_interesantes_perimetro(const unsigned char perimetro)
+// Escanea las casillas del perímetro p y guarda los resultados en el mapa de potencial y las colas correspondientes
+bool ComportamientoJugador::escanear_perimetro(const unsigned char perimetro)
 {
 	bool encontrada = false;
-	for (size_t i = (posicion.first - perimetro); i < (posicion.first + perimetro); i++)
-	{
-		for (size_t j = (posicion.second - perimetro); j < (posicion.second + perimetro); j++)
-		{
-			if (es_casilla_interesante(make_pair(i, j), mapaResultado))
-				encontrada = true;
-			else if (es_casilla_desconocida(make_pair(i, j), mapaResultado))
-				encontrada = true;
-		}
-	}
 
+	for (size_t i = 0; i < (perimetro * 2); i++)
+	{
+		// Borde superior
+		if (escanear_casilla(make_pair(estado->fil - perimetro, (estado->col - perimetro) + i)))
+			encontrada = true;
+		// Borde inferior
+		if (escanear_casilla(make_pair(estado->fil + perimetro, (estado->col - perimetro) + i)))
+			encontrada = true;
+		// Borde izquierdo
+		if (escanear_casilla(make_pair((estado->col - perimetro) + i , estado->col - perimetro)))
+			encontrada = true;
+		// Borde derecho
+		if (escanear_casilla(make_pair((estado->col - perimetro) + i, estado->col + perimetro)))
+			encontrada = true;
+	}
+	
 	return encontrada;
 }
 
-void ComportamientoJugador::generar_mapa_potencial_parcial(const unsigned char radio)
+// Genera el mapa de decisión con un radio inteligente según el mapa de potencial (debe estar relleno)
+void ComportamientoJugador::generar_mapa_decision(const unsigned char radio, Sensores sensor)
 {
+	// Borro el mapa de decisión, si es que tuviese algo, para rellenarlo de nuevo
+    for (auto& row : mapa_decision)
+        row.clear();
 
+	int distance;
+	int optimal_size = radio;
+	casilla c;
+
+	// Buscamos la máxima distancia a la que se encuentra algún elemento interesante para calcular el tamaño de la matriz
+	for (size_t i = 0; i < casillas_interesantes.size(); i++)
+	{
+		c = casillas_interesantes.front();
+		distance = c.first > estado->fil ? c.first - estado->fil : estado->fil - c.first;
+		optimal_size = optimal_size > distance ? optimal_size : distance;
+
+		distance = c.second > estado->col ? c.second - estado->col : estado->col - c.second;
+		optimal_size = optimal_size > distance ? optimal_size : distance;
+
+		casillas_interesantes.push(c);
+		casillas_interesantes.pop();
+	}
+	mapa_decision = vector<vector<int>>(optimal_size, vector<int>(optimal_size, 0));
+
+	// Calculamos el efecto que tienen las casillas interesantes sobre el mapa de potencial
+	for (size_t i = 0; i < casillas_interesantes.size(); i++)
+	{
+		c = casillas_interesantes.front();
+
+		mapa_decision[c.first][c.second] = sensor.bateria + mapa_decision[c.first][c.second];
+		calcular_efecto_entorno(c, sensor.bateria);
+
+		casillas_interesantes.push(c);
+		casillas_interesantes.pop();
+	}
+	
+	// Lo mismo pero para las casillas desconocidas
+	for (size_t i = 0; i < casillas_desconocidas.size(); i++)
+	{
+		c = casillas_desconocidas.front();
+
+		mapa_decision[c.first][c.second] = sensor.bateria + mapa_decision[c.first][c.second];
+		calcular_efecto_entorno(c, sensor.bateria);
+
+		casillas_desconocidas.push(c);
+		casillas_desconocidas.pop();
+	}
+}
+// Complemento para la función "generar_mapa_decision"
+void ComportamientoJugador::calcular_efecto_entorno(const casilla c, const int bateria)
+{
+	for (int i = -1; i < 2; i++)
+	{
+		for (int j = -1; j < 2; j++)
+		{
+			if (i || j)
+			{
+				int potencial = (*mapa_potencial_actual)[c.first + i][c.second + j];
+
+				if (potencial){ 
+					if (mapa_decision[c.first + i][c.second + j] < potencial + bateria){
+						mapa_decision[c.first + i][c.second + j] = potencial + bateria;
+						calcular_efecto_entorno(make_pair(c.first + i, c.second + j), bateria);
+					}
+				}
+			}
+		}
+	}
 }
 
 template <typename T>
 void ComportamientoJugador::trasladar_mapa(vector< vector<T>>& base, const vector< vector<T>>& objetivo)
 {
+	if (fake_state.brujula < current_state.brujula)
+		fake_state.brujula += 8;
+	
 	const short offset_fil = fake_state.fil - current_state.fil;
 	const short offset_col = fake_state.col - current_state.col;
 
@@ -82,7 +153,6 @@ void ComportamientoJugador::PonerTerrenoEnMatriz(const vector<unsigned char> ter
 	short posfil, poscol;
 	bool swap;
 
-	cout << "Brujula: " << current_state.brujula << endl;
 	switch (current_state.brujula)
 	{
 	case norte: 	posfil = 1; 	poscol = 1;		vector_pos = pos_norte; 	swap = false;	break;
@@ -112,74 +182,121 @@ Action ComportamientoJugador::think(Sensores sensores)
 	switch (last_action)
 	{
 	case actWALK:
-		switch (current_state.brujula)
+		switch (estado->brujula)
 		{
-			case norte: current_state.fil -= walk_stepsize; break;
-			case noreste: current_state.fil -= walk_stepsize; current_state.col += walk_stepsize; break;
-			case este: current_state.col += walk_stepsize; break;
-			case sureste: current_state.fil += walk_stepsize; current_state.col += walk_stepsize; break;
-			case sur: current_state.fil += walk_stepsize; break;
-			case suroeste: current_state.fil += walk_stepsize; current_state.col -= walk_stepsize; break;
-			case oeste: current_state.col -= walk_stepsize; break;
-			case noroeste: current_state.fil -= walk_stepsize; current_state.col -= walk_stepsize; break;
+			case norte: estado->fil -= walk_stepsize; break;
+			case noreste: estado->fil -= walk_stepsize; estado->col += walk_stepsize; break;
+			case este: estado->col += walk_stepsize; break;
+			case sureste: estado->fil += walk_stepsize; estado->col += walk_stepsize; break;
+			case sur: estado->fil += walk_stepsize; break;
+			case suroeste: estado->fil += walk_stepsize; estado->col -= walk_stepsize; break;
+			case oeste: estado->col -= walk_stepsize; break;
+			case noroeste: estado->fil -= walk_stepsize; estado->col -= walk_stepsize; break;
 		}
 		break;
 
 	case actRUN:
-		switch (current_state.brujula)
+		switch (estado->brujula)
 		{
-			case norte: current_state.fil -= run_stepsize; break;
-			case noreste: current_state.fil -= run_stepsize; current_state.col += run_stepsize; break;
-			case este: current_state.col += run_stepsize; break;
-			case sureste: current_state.fil += run_stepsize; current_state.col += run_stepsize; break;
-			case sur: current_state.fil += run_stepsize; break;
-			case suroeste: current_state.fil += run_stepsize; current_state.col -= run_stepsize; break;
-			case oeste: current_state.col -= run_stepsize; break;
-			case noroeste: current_state.fil -= run_stepsize; current_state.col -= run_stepsize; break;
+			case norte: estado->fil -= run_stepsize; break;
+			case noreste: estado->fil -= run_stepsize; estado->col += run_stepsize; break;
+			case este: estado->col += run_stepsize; break;
+			case sureste: estado->fil += run_stepsize; estado->col += run_stepsize; break;
+			case sur: estado->fil += run_stepsize; break;
+			case suroeste: estado->fil += run_stepsize; estado->col -= run_stepsize; break;
+			case oeste: estado->col -= run_stepsize; break;
+			case noroeste: estado->fil -= run_stepsize; estado->col -= run_stepsize; break;
 		}
 		break;
 
 	case actTURN_SR:
 		// Girar 45º derecha
-		current_state.brujula = static_cast<Orientacion> ((current_state.brujula+1)%8);
+		estado->brujula = static_cast<Orientacion> ((estado->brujula+1)%8);
 		break;
 	
 	case actTURN_L:
 		// Girar 90º izquierda
-		current_state.brujula = static_cast<Orientacion> ((current_state.brujula+6)%8);
+		estado->brujula = static_cast<Orientacion> ((estado->brujula+6)%8);
 		break;
 	}
 
-	if (sensores.terreno[0] == 'G' and !bien_situado)
+	int porcentaje_bateria = static_cast<int>((static_cast<float>(sensores.bateria) / bateria_maxima) * 100);
+	if (porcentaje_bateria < 50)
+		prioridad_recarga = 6000 - ((bateria_maxima*porcentaje_bateria)/100);
+	else
+		prioridad_recarga = -1;
+	
+	// Si encuentro alguna pieza valiosa lo registro
+	if (sensores.terreno[0] == 'G' and !ubicado)
 	{
-		current_state.fil = sensores.posF;
-		current_state.col = sensores.posC;
-		current_state.brujula = sensores.sentido;
-		bien_situado = true;
+		mapa_actual = &mapaResultado;
+		mapa_potencial_actual = &mapa_potencial;
+		estado = &current_state;
+		estado->fil = sensores.posF;
+		estado->col = sensores.posC;
+		estado->brujula = sensores.sentido;
+		ubicado = true;
+		trasladar_mapa(mapa_potencial_temporal, mapa_potencial);
+		trasladar_mapa(mapa_resultado_temporal, mapaResultado);
 	}
-
-	if (bien_situado)
-	{
-		cout << "Se ha entrado en poner terreno" << endl;
-		PonerTerrenoEnMatriz(sensores.terreno, current_state, mapaResultado, sensores.nivel);
-	}
+	if (sensores.terreno[0] == 'K' and !bikini)
+		bikini = true;
+	if (sensores.terreno[0] == 'D' and !zapatillas)
+		zapatillas = true;
+	if (sensores.terreno[0] == 'X')
+		recarga = recarga ? recarga - 1 : 5;
+	
+	// Registro lo que veo
+	PonerTerrenoEnMatriz(sensores.terreno, *estado, *mapa_actual, sensores.nivel);
 	
 	// Decidir nueva acción
-	if ((sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S' or
-		sensores.terreno[2] == 'G') and sensores.agentes[2] == '_')
+	if (prioridad_recarga != -1 && recarga)
 	{
+		accion = actIDLE;
+	}
+	
+	switch (estado->brujula)
+	{
+		case norte: frente = break;
+		case noreste: break;
+		case este: break;
+		case sureste: break;
+		case sur: break;
+		case suroeste: break;
+		case oeste: break;
+		case noroeste: break;
+
+	int mejor_valor = 0;
+	unsigned char radio_vision = 1;
+	do
+	{
+		while (!escanear_perimetro(radio_vision))
+		{
+			radio_vision++;
+		}
+		
+		generar_mapa_decision(radio_vision, sensores);
+
+		// Elijo la mejor opción
+		int penalizacion = (*mapa_potencial_actual)[estado->fil][estado->col];
+		//casilla mejor_opcion = make_pair(estado->fil + frente, estado->col + frente);
+		mejor_valor = mapa_decision[estado->fil + frente][estado->col + frente];
 		accion = actWALK;
-	}
-	else if (!girar_derecha)
-	{
-		accion = actTURN_L;
-		girar_derecha = (rand()%2 == 0);
-	}
-	else
-	{
-		accion = actTURN_SR;
-		girar_derecha = (rand()%2 == 0);
-	}
+
+		if (mapa_decision[estado->fil + izquierda][estado->col + izquierda] - penalizacion > mejor_valor){
+			accion = actTURN_L;
+			mejor_valor = mapa_decision[estado->fil + izquierda][estado->col + izquierda] - penalizacion;
+		}
+
+		if (mapa_decision[estado->fil + derecha][estado->col + derecha] - penalizacion > mejor_valor){
+			accion = actTURN_SR;
+			mejor_valor = mapa_decision[estado->fil + derecha][estado->col + derecha] - penalizacion;
+		}
+
+		radio_vision++;
+		
+	} while (mejor_valor == 0);
+	
 	
 	last_action = accion;
 
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -21,20 +21,29 @@ class ComportamientoJugador : public Comportamiento
 {
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
-      // Constructor de la clase
-      // Dar el valor inicial a las variables de estado
+      zapatillas = false;
+      bikini = false;
+
       current_state.fil = 99;
       current_state.col = 99;
       current_state.brujula = norte;
 
-      ubicado = false;
-
-      mapa_resultado_temporal[mapaResultado.size()*2][mapaResultado.size()*2];
-      mapa_actual = &mapa_resultado_temporal;
-
-      fake_state.fil = mapa_resultado_temporal.size() / 2;
+      fake_state.fil = mapa_size / 2;
       fake_state.col = fake_state.fil;
       fake_state.brujula = norte;
+
+      estado = &fake_state;
+      ubicado = false;
+      prioridad_recarga = -1;
+      bateria_maxima = 15000;
+      recarga = 0;
+
+      mapa_resultado_temporal = vector< vector< unsigned char> >(mapa_size*2, vector<unsigned char>(mapa_size, '?'));
+      mapa_potencial_temporal = vector<vector<int>>(mapa_size*2, vector<int>(mapa_size, 1));
+      mapa_potencial =          vector<vector<int>>(mapa_size, vector<int>(mapa_size, 1));
+
+      mapa_actual = &mapa_resultado_temporal;
+      mapa_potencial_actual = &mapa_potencial_temporal;
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -45,37 +54,31 @@ class ComportamientoJugador : public Comportamiento
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
 
-    bool es_casilla_interesante(const casilla c);
-    bool es_casilla_desconocida(const casilla c);
-    bool buscar_casillas_interesantes_perimetro(const unsigned char perimetro);
-    void generar_mapa_potencial_parcial(const unsigned char radio);
+    bool escanear_casilla(const casilla c);
+    bool escanear_perimetro(const unsigned char perimetro);
+    void generar_mapa_decision(const unsigned char radio, Sensores sensor);
+    void calcular_efecto_entorno(const casilla c, const int bateria);
     template <typename T>
     void trasladar_mapa(vector< vector<T>>& base, const vector< vector<T>>& objetivo);
 
   private:
   // Declarar aquí las variables de estado
-  state current_state, fake_state; Orientacion brujula;
+  state current_state, fake_state, *estado; Orientacion brujula;
   Action last_action;
-  bool ubicado;
+  bool ubicado, zapatillas, bikini;
+  const unsigned short mapa_size = mapaResultado.size();
+  unsigned char recarga;
+  int prioridad_recarga, bateria_maxima;
 
-/*
-  struct PairHash {
-    size_t operator()(const casilla& p) const {
-      return hash<unsigned char>()(p.first) ^ hash<unsigned char>()(p.second);
-    }
-  };
-
-  unordered_map<casilla, unsigned char, PairHash> casillas_interesantes;
-  unordered_set<casilla> casillas_desconocidas;
-*/
-  queue<pair<casilla, unsigned char>> casillas_interesantes;
+  queue<casilla> casillas_interesantes;
   queue<casilla> casillas_desconocidas;
-  vector< vector< unsigned char> > mapa_resultado_temporal;
-  vector< vector< unsigned short> > mapa_potencial_temporal;
-  vector< vector< unsigned short> > mapa_potencial;
-  vector< vector< unsigned short> > mapa_decision;
 
-  vector< vector< unsigned char> > *mapa_actual;
+  vector<vector<unsigned char>> mapa_resultado_temporal;
+  vector<vector<int>> mapa_potencial_temporal;
+  vector<vector<int>> mapa_potencial;
+  vector<vector<int>> mapa_decision;
+  vector<vector<unsigned char>> *mapa_actual;
+  vector<vector<int>> *mapa_potencial_actual;
 };
 
 #endif
